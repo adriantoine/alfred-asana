9c024af74b10902ab8865a9ffadd1abfe3dba100
Adrien Antoine <adriantoine@gmail.com>
2017-07-30 18:21:53 +0100

    Added Asignee and Project name to subtitle

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 const alfy = require('alfy');
+const isNil = require('lodash.isnil');
 const alfredNotifier = require('alfred-notifier');
 
 alfredNotifier();
@@ -7,18 +8,42 @@ alfredNotifier();
 const WORKSPACE_ID = process.env.WORKSPACE_ID;
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
-alfy.fetch(`https://app.asana.com/api/1.0/workspaces/${WORKSPACE_ID}/typeahead`, {
-	headers: {Authorization: `Bearer ${ACCESS_TOKEN}`},
-	query: {
-		query: alfy.input,
-		type: 'task'
-	}
-})
-	.then(data => {
-		const items = data.data.map(x => ({
-			title: x.name,
-			arg: `${WORKSPACE_ID}/${x.id}`
-		}));
+const getProjectNames = projects =>
+  `Projects: ${projects.map(proj => proj.name).join(', ')}`;
 
-		alfy.output(items);
-	});
+const getAssignee = assignee => `Assigned to ${assignee.name}`;
+
+const getSubtitle = task => {
+  let subtitle = '';
+
+  if (!isNil(task.projects)) {
+    subtitle += getProjectNames(task.projects);
+  }
+
+  if (!isNil(task.assignee)) {
+    subtitle += ` | ${getAssignee(task.assignee)}`;
+  }
+
+  return subtitle;
+};
+
+alfy
+  .fetch(`https://app.asana.com/api/1.0/workspaces/${WORKSPACE_ID}/typeahead`, {
+    headers: {Authorization: `Bearer ${ACCESS_TOKEN}`},
+    query: {
+      query: alfy.input,
+      type: 'task',
+      opt_fields: 'id,name,this.assignee.name,this.projects.name'
+    }
+  })
+  .then(data =>
+    alfy.output(
+      data.data.map(x => ({
+        uid: `${WORKSPACE_ID}/${x.id}`,
+        title: x.name,
+        autocomplete: x.name,
+        subtitle: getSubtitle(x),
+        arg: `${WORKSPACE_ID}/${x.id}`
+      }))
+    )
+  );
